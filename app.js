0ae5a9fa8315ca5ae506626c496cc75eafa775d1
Rui Silva <ruisinhofilipe@gmail.com>
2023-06-24 16:47:54 +0200

    JS of header

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const leftBar = document.querySelector("#leftTopBar");
 const bookButton = document.querySelector("#btn_nav");
 const hamburguerButton = document.querySelector(".hamburguerButton");
 
+// RESIZE PAGE WIDTH AND DISPLAY NAV BAR
 window.addEventListener("load", handleWindowSizeChange);
+window.addEventListener("resize", handleWindowSizeChange);
 
 function handleWindowSizeChange() {
   width = window.innerWidth;
@@ -33,4 +35,55 @@ function displayHamburguerMenu() {
   });
 }
 
-window.addEventListener("resize", handleWindowSizeChange);
+// CHANGE HEADER BACKGROUND IMAGE WITH USER CLICK OR EACH 10SECONDS.
+
+const firstImg = "/img/gallery/slide-1.jpg";
+const secondImg = "/img/gallery/slide-2.jpg";
+const thirdImg = "/img/gallery/slide-4.jpg";
+const fourthImg = "/img/gallery/slide-3.jpg";
+const header = document.querySelector("header");
+let currentImg = 2;
+const changeImgButton = document.querySelectorAll(".headerB");
+
+// STORE EACH IMG INSIDE AN ARRAY
+const imgArray = [];
+imgArray.push(firstImg, secondImg, thirdImg, fourthImg);
+
+// EACH CLICK CHANGE BACKGROUND IMAGE
+changeImgButton.forEach((button) => {
+  button.addEventListener("click", () => {
+    currentImg = button.getAttribute("data-index");
+    //CHANGE BACKGROUND IMAGE
+    header.style.backgroundImage = `url(${
+      imgArray[button.getAttribute("data-index")]
+    })`;
+
+    //CHANGE CLASS
+    changeImgButton.forEach((button) => {
+      if (button.getAttribute("data-index") === currentImg) {
+        button.classList.add("active");
+      } else {
+        button.classList.remove("active");
+      }
+    });
+  });
+});
+
+// CHANGE BACKGROUND IMAGe EACH 10 SEC
+function changeImgEachTenS() {
+  let i = currentImg + 1;
+  currentImg++;
+  currentImg === 3 ? (currentImg = -1) : (currentImg = currentImg);
+
+  header.style.backgroundImage = `url(${imgArray[i]})`;
+
+  // CHANGE CLASS OF BUTTONS
+  for (let j = 0; j < changeImgButton.length; j++) {
+    if (parseInt(changeImgButton[j].getAttribute("data-index")) !== i) {
+      changeImgButton[j].classList.remove("active");
+    } else {
+      changeImgButton[j].classList.add("active");
+    }
+  }
+}
+setInterval(changeImgEachTenS, 10000);
