35da508031e854171cc9a2b7584af216b557715d
Sarah Bourgeois <https://github.com/Sarah2511>
2023-06-26 14:02:31 +0200

    homepage + navbar resposive the homepage is not done

--- script/nav.js
+++ script/nav.js
@@ -1,21 +1,6 @@
-const burger = document.getElementById('burger');
-let menu = document.getElementById('nav_links');
-burger.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    burger.classList.toggle('active');
-});
+const menuHamburger = document.querySelector(".menu-burger")
+const navLinks = document.querySelector(".ul-menu")
 
-let links = document.getElementsByClassName('links_menu');
-
-for (let link of links) {
-    link.addEventListener('click', () => {
-        for (let linkColorBase of links) {
-            linkColorBase.style.color = '#CBCBCB';
-        }
-        link.style.color = 'white';
-        for (let ligneRed of links) {
-            ligneRed.parentElement.querySelector('.ligne_red').style.width = '0';
-        }
-        link.parentElement.querySelector('.ligne_red').style.width = '100%';
-    });
-}
+menuHamburger.addEventListener('click',()=>{
+navLinks.classList.toggle('mobile-menu')
+})
